e6db75bddc26896d58de48caddac5dcd17ac166f
bodyanept <red.suka.lantern@gmail.com>
2024-11-05 21:22:04 +0500

    first commit

---
+++ dz2.js
@@ -0,0 +1,14 @@
+//1task
+
+let age = prompt('сколько тебе лет?')
+if(age >=0 && age <=2){
+    console.log('младенец')}
+else if(age >=12 && age <=18 ){
+    console.log('подросток')}
+else if(age >=18 && age <=60 ){
+    console.log('вы взрослый')}
+else if(age >=60 ){
+    console.log('вы пенсионер')}
+
+
+    //2task
